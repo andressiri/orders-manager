746a7c0bc6e8647a57f8d7cb31813364a5b1c0b2
Andrés Siri <andres.siri@hotmail.com>
2023-07-23 21:14:07 -0300

    OM-1 Update getOrders use case

--- server/application/useCases/orders/getOrders.ts
+++ server/application/useCases/orders/getOrders.ts
@@ -1,10 +1,13 @@
-import { IOrder } from "../../../domain/typings/order";
+import { IOrderModel } from "../../../domain/typings/order";
 import { throwHTTPError } from "../../helpers";
 
-const getOrders = (ordersSearch: IOrder[]) => {
-  if (ordersSearch.length < 1) {
+const getOrders = async (Order: IOrderModel) => {
+  const ordersSearch = await Order.getOrders();
+
+  if (!ordersSearch || ordersSearch?.length < 1)
     throwHTTPError(404, "No orders found");
-  }
+
+  return ordersSearch;
 };
 
 export default getOrders;
