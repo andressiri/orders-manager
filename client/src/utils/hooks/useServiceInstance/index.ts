639043488787565baef3ba72e55e89cf28bcaa72
Andrés Siri <andres.siri@hotmail.com>
2023-07-24 10:38:37 -0300

    OM-2 Create useServiceInstance hook

---
+++ client/src/utils/hooks/useServiceInstance/index.ts
@@ -0,0 +1,81 @@
+import { useCallback, useState } from "react";
+import { makeRequest } from "../../../utils/helpers";
+import { EntitiesState /* SetServiceState */ } from "../../../typings/contexts";
+import {
+  ExecuteProps,
+  IHandleResponseOptions,
+} from "../../../typings/services";
+import useHandleResponseEffect from "../useHandleResponseEffect";
+
+const useServiceInstance = (responseOptions: IHandleResponseOptions) => {
+  const [isError, setIsError] = useState(false);
+  const [isSuccess, setIsSuccess] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const [message, setMessage] = useState("");
+
+  useHandleResponseEffect({
+    ...responseOptions,
+    serviceState: {
+      isError,
+      setIsError,
+      isSuccess,
+      setIsSuccess,
+      message,
+      setMessage,
+    },
+  });
+
+  const executeRequest = useCallback(
+    async ({
+      route,
+      baseUrl,
+      data,
+      method,
+      contentType,
+      headers,
+      setState,
+    }: ExecuteProps) => {
+      const notGetRequest = method && method !== "GET";
+      setIsLoading(true);
+
+      try {
+        const requestObj = {
+          route,
+          baseUrl,
+          data,
+          method,
+          contentType,
+          headers,
+        };
+
+        const response = await makeRequest(requestObj);
+
+        if (setState) {
+          const dataToSet: EntitiesState = notGetRequest
+            ? response.data
+            : response;
+          setState(dataToSet);
+        }
+
+        if (notGetRequest) setMessage(response.message);
+        setIsSuccess(true);
+      } catch (err) {
+        setMessage(`${err}`);
+        setIsError(true);
+      } finally {
+        setIsLoading(false);
+      }
+    },
+    [setIsLoading],
+  );
+
+  return {
+    executeRequest,
+    isError,
+    isSuccess,
+    isLoading,
+    message,
+  };
+};
+
+export default useServiceInstance;
